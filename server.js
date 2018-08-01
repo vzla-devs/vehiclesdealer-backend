b9d5be2aa4c47a1ae831e1d79e6cfc9dac568172
Gabriel Nuñez <gabriel.nzn@gmail.com>
2018-08-01 15:23:52 +0100

    cambiando el puerto del servidor

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
-const port = 3000
+const port = 8080
 const cors = require('cors')
 app.use('/static', express.static('uploads'));
 
