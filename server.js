720f64a345468b73b178c32652d23883c912c030
Gabriel Nuñez <gabriel.nzn@gmail.com>
2018-08-03 13:54:08 +0100

    Revisando el archivo server.js

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 // express
 const express = require('express')
-const bodyParser = require('body-parser')
+// const bodyParser = require('body-parser')
 const app = express()
-const port = 8080
+const port = 8000
 const cors = require('cors')
-app.use('/static', express.static('uploads'));
+app.use('/api/static', express.static('uploads'))
 
 app.use(cors()) // para manejar solicitudes Cross-origin
 //app.use(bodyParser.json()) // para parsear json
@@ -14,12 +14,12 @@ app.use(cors()) // para manejar solicitudes Cross-origin
 var mongoose = require('mongoose')
 const mongoDB = 'mongodb://localhost:27017/laherradura'
 mongoose.connect(mongoDB, { useNewUrlParser: true })
-mongoose.Promise = global.Promise;
+mongoose.Promise = global.Promise
 const db = mongoose.connection
 
 // rutas
 const cars = require('./routes/cars')
-app.use('/coches', cars)
+app.use('/api/coches', cars)
 
 // conexión con mongoDB
 db.on('error', console.error.bind(console, 'Error de conexión:'));
