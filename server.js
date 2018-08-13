48c6ed00c930d4974ad8bb9b97ac9b38950d7561
Gabriel Nuñez <gabriel.nzn@gmail.com>
2018-08-13 16:50:54 +0100

    cambiando el nombre de la base de datos a conectarse

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ app.use(bodyParser.urlencoded({ extended: true })) // para parsear application/x
 
 // mongoose
 var mongoose = require('mongoose')
-const mongoDB = 'mongodb://localhost:27017/laherradura'
+const mongoDB = 'mongodb://localhost:27017/moncars'
 mongoose.connect(mongoDB, { useNewUrlParser: true })
 mongoose.Promise = global.Promise
 const db = mongoose.connection
