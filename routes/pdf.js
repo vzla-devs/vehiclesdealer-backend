a77df23ce5a27ab7cf3c82609cf7f82eea184a10
Gabriel Nuñez <gabriel.nzn@gmail.com>
2018-09-25 12:56:54 +0100

    .

--- routes/pdf.js
+++ routes/pdf.js
@@ -22,12 +22,12 @@ const storage = multer.diskStorage({
 })
 
 router.get('/:picture', (req, res) => {
-
+    const file = req.params.picture
     const dotIndex = file.indexOf('.')
     const format = file.substring(dotIndex + 1, file.length)
     const appendData = `data:image/${format};base64,`
 
-    fs.readFile(`uploads/${req.params.picture}`, (err, data) => {
+    fs.readFile(`uploads/${file}`, (err, data) => {
         if (err) return res.status(500).send(err)
         const base64 = appendData + data.toString('base64')
         return res.status(200).send({ base64 })
