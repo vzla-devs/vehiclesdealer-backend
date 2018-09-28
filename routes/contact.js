9fd0ea566780546cd1cf8068b9387dbb5b72a165
Gabriel Nuñez <gabriel.nzn@gmail.com>
2018-09-28 22:29:26 +0100

    corrigiendo endpoint para actualizar la información de contacto

--- routes/contact.js
+++ routes/contact.js
@@ -23,6 +23,43 @@ router.put('/', async(req, res) => {
         saturday
     } = req.body
 
+    Contact.findOne({})
+    .exec(async(err, contact) => {
+        if (err) return res.status(500).send(err)
+
+        // si la información de contacto no existe en la base de datos
+        if (contact !== null) {
+            contact.mobilePhone = mobilePhone
+            contact.mainPhone = mainPhone
+            contact.emails = emails
+            contact.monday = monday
+            contact.tuesday = tuesday
+            contact.wednesday = wednesday
+            contact.thursday = thursday
+            contact.friday = friday
+            contact.saturday = saturday
+        } else {
+            contact = new Contact ({
+                mobilePhone: mobilePhone,
+                mainPhone: mainPhone,
+                emails: emails,
+                monday: monday,
+                tuesday: tuesday,
+                wednesday: wednesday,
+                thursday: thursday,
+                friday: friday,
+                saturday: saturday
+            })
+        }
+
+        try {
+            contact = await contact.save()
+            res.status(200).send(contact)
+        } catch (err) {
+           res.status(500).send(err)
+        }
+    })
+
     let contact = new Contact ({
         mobilePhone: mobilePhone,
         mainPhone: mainPhone,
