2f1e9a7db1dc54d151a380b2ff2f3c59ae2ebef2
Gabriel Nunez <gnunez@aidacanarias.com>
2020-05-02 19:36:32 +0100

    ignore

--- src/infrastructure/persistenceFactory.js
+++ src/infrastructure/persistenceFactory.js
@@ -3,7 +3,6 @@ import mongoose from 'mongoose'
 function getDatabaseConnection () {
   const connectionString = 'mongodb://localhost:32768/vehiclesdealer'
   mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
-  mongoose.Promise = global.Promise
   return mongoose.connection
 }
 
