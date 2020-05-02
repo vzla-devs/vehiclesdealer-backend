352a130670c52dc17fed9bb75e39e23a4fd3565f
Gabriel Nunez <gnunez@aidacanarias.com>
2020-05-02 20:05:33 +0100

    refactor: extracting methods

--- src/index.js
+++ src/index.js
@@ -3,9 +3,17 @@ import { connectToDatabase } from './infrastructure/persistenceFactory'
 import { createWebApplication } from './infrastructure/applicationFactory'
 
 const databaseConnection = connectToDatabase()
-databaseConnection.on('error', console.error.bind(console, 'Hubo un error de conexión con la base de datos: '))
-databaseConnection.once('open', () => {
-  const app = createWebApplication()
-  const port = 8000
-  app.listen(port, () => console.log(`Escuchando en el puerto ${port}`))
-})
+logDatabaseErrors()
+runAppOnceTheDatabaseIsConnected()
+
+function logDatabaseErrors () {
+  databaseConnection.on('error', console.error.bind(console, 'Hubo un error de conexión con la base de datos: '))
+}
+
+function runAppOnceTheDatabaseIsConnected () {
+  databaseConnection.once('open', () => {
+    const app = createWebApplication()
+    const port = 8000
+    app.listen(port, () => console.log(`Escuchando en el puerto ${port}`))
+  })
+}
