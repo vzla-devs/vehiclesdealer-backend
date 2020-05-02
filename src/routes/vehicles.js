0a6cef526d478d4607962b051b5b36fc352a5e6b
Gabriel Nunez <gnunez@aidacanarias.com>
2020-05-02 20:18:26 +0100

    removing unused dependency

--- src/routes/vehicles.js
+++ src/routes/vehicles.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router()
 const Vehicle = require('../models/vehicle')
 const fs = require('fs')
-const path = require('path')
 const multer = require('multer')
 const sharp = require('sharp')
 const storage = multer.diskStorage({
