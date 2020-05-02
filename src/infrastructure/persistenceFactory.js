53ae3eba83237ec633240054a0957ac41e7067ad
Gabriel Nunez <gnunez@aidacanarias.com>
2020-05-02 19:41:33 +0100

    refactor: extracting methods

--- src/infrastructure/persistenceFactory.js
+++ src/infrastructure/persistenceFactory.js
@@ -2,7 +2,15 @@ import mongoose from 'mongoose'
 
 function getDatabaseConnection () {
   const connectionString = 'mongodb://localhost:32768/vehiclesdealer'
+  connectToDatabase(connectionString)
+  return getConnection()
+}
+
+function connectToDatabase (connectionString) {
   mongoose.connect(connectionString, { useNewUrlParser: true, useUnifiedTopology: true })
+}
+
+function getConnection () {
   return mongoose.connection
 }
 
