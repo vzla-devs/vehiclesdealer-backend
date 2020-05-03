1b7597c63cf8f6af043ec3e5b3315b0d735a6184
Gabriel Nunez <gnunez@aidacanarias.com>
2020-05-03 12:40:38 +0100

    removing code

--- src/application/editVehiclePicturesAction.js
+++ src/application/editVehiclePicturesAction.js
@@ -1,20 +1,8 @@
-import fs from 'fs'
 import Vehicle from '@/domain/models/vehicle'
 
 async function execute (command) {
   const vehicle = await Vehicle.findOne({ _id: command.id }).exec()
   if (!vehicle) throw new Error('el vehículo no existe')
-  command.files.forEach(file => {
-    sharp(`uploads/${file.filename}`)
-    .withMetadata()
-    .resize(1920, 1080)
-    .toBuffer(`uploads/${file.filename}`, (err, data) => {
-        if (err) throw err
-        fs.writeFile(`uploads/${file.filename}`, data, 'binary', err => {
-            if (err) throw err
-        })
-    })
-  })
   if (command.files.length > 0) {
     const newPictures = command.files.map(pic => `${pic.filename}`)
     vehicle.pictures = [...vehicle.pictures, ...newPictures]
