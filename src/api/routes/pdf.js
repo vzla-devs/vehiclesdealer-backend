5187cfde6a85ad6bc47a910e06cb71b05aae637d
Gabriel Nunez <gnunez@aidacanarias.com>
2020-05-04 13:03:21 +0100

    rename

--- src/api/routes/pdf.js
+++ src/api/routes/pdf.js
@@ -6,11 +6,11 @@ router.get('/:picture', (req, res) => {
     const file = req.params.picture
     const dotIndex = file.indexOf('.')
     const format = file.substring(dotIndex + 1, file.length)
-    const appendData = `data:image/${format};base64,`
+    const pictureFormatData = `data:image/${format};base64,`
 
     fs.readFile(`public/uploads/${file}`, (err, data) => {
         if (err) return res.status(500).send(err)
-        const base64 = appendData + data.toString('base64')
+        const base64 = pictureFormatData + data.toString('base64')
         return res.status(200).send({
             base64,
             format
