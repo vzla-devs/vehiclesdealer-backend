10668b3e5d04a7ef9413bfa25532916e04082f39
Gabriel Nunez <gnunez@aidacanarias.com>
2020-05-04 13:05:17 +0100

    renaming variables

--- src/api/routes/pdf.js
+++ src/api/routes/pdf.js
@@ -3,18 +3,15 @@ const router = express.Router()
 const fs = require('fs')
 
 router.get('/:picture', (req, res) => {
-    const file = req.params.picture
-    const dotIndex = file.indexOf('.')
-    const format = file.substring(dotIndex + 1, file.length)
-    const pictureFormatData = `data:image/${format};base64,`
+    const pictureToGet = req.params.picture
+    const dotSeparatorPosition = pictureToGet.indexOf('.')
+    const pictureFormat = pictureToGet.substring(dotSeparatorPosition + 1, pictureToGet.length)
+    const pictureFormatData = `data:image/${pictureFormat};base64,`
 
-    fs.readFile(`public/uploads/${file}`, (err, data) => {
+    fs.readFile(`public/uploads/${pictureToGet}`, (err, data) => {
         if (err) return res.status(500).send(err)
         const base64 = pictureFormatData + data.toString('base64')
-        return res.status(200).send({
-            base64,
-            format
-        })
+        return res.status(200).send({ base64, format: pictureFormat })
       })
 })
 
