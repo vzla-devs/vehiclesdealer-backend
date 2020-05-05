831e09432e92f36d21872d07bad3054bdf489dcc
Gabriel Nunez <gnunez@aidacanarias.com>
2020-05-05 11:50:47 +0100

    removing import

--- src/api/routes/contact.js
+++ src/api/routes/contact.js
@@ -1,12 +1,11 @@
 import express from 'express'
-import Contact from '@/domain/models/contact'
 import { getContacQuery } from '@/application/contact/getContacQuery'
 import { changeContactAction } from '@/application/contact/changeContactAction'
 import { tryThis } from '@/api/decorators'
 
 const router = express.Router()
 
-router.get('/', tryThis((req, res) => {
+router.get('/', tryThis(async(req, res) => {
     const contact = await getContacQuery.get()
     res.status(200).send(contact)
 }))
