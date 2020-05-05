84186dfebbbc6115b1b48299bbd61265b9cb6c2b
Gabriel Nunez <gnunez@aidacanarias.com>
2020-05-05 22:01:03 +0100

    ignore

--- src/infrastructure/usersRepositoryMongoDB.ts
+++ src/infrastructure/usersRepositoryMongoDB.ts
@@ -3,7 +3,7 @@ import { User } from '@/domain/models/user'
 
 class UsersRepositoryMongoDB implements UsersRepository {
   database: any
-  
+
   async create(userToCreate: User) {
     const users = this.database.collection('users')
     const userData = userToCreate.getCredentials()
