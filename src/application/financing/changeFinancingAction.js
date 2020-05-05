5b25eecf21281a8e9068984a0ce0ad4899403243
Gabriel Nunez <gnunez@aidacanarias.com>
2020-05-05 11:43:53 +0100

    rename

--- src/application/financing/changeFinancingAction.js
+++ src/application/financing/changeFinancingAction.js
@@ -3,8 +3,8 @@ import Financing from '@/domain/models/financing'
 async function execute (command) {
   const existingFinancing = await Financing.findOne({}).exec()
   if (!existingFinancing) {
-    const financingToCreate = new Financing ({ amount: command.amount })
-    await financingToCreate.save()
+    const financingToAdd = new Financing ({ amount: command.amount })
+    await financingToAdd.save()
   } else {
     existingFinancing.amount = command.amount
     await existingFinancing.save()
