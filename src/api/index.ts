1a01dcf39bee763d89a994a9a8f106f1aa5c8713
Gabriel Nunez <gnunez@aidacanarias.com>
2020-05-06 12:58:54 +0100

    shutting down the application gracefully

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,18 +1,38 @@
 import { connectToDatabase } from '@/infrastructure/persistenceFactory'
 import { createWebApplication } from '@/infrastructure/applicationFactory'
 
+let server
 const databaseConnection = connectToDatabase()
 runAppOnceTheDatabaseIsConnected()
 logDatabaseErrors()
 
-function runAppOnceTheDatabaseIsConnected () {
-  databaseConnection.once('open', () => {
+function runAppOnceTheDatabaseIsConnected (): void {
+  return databaseConnection.once('open', () => {
     const app = createWebApplication()
     const port = 8000
-    app.listen(port, () => console.log(`Escuchando en el puerto ${port}`))
-  })
+    server = app.listen(port, () => console.log('Running...'))
+  }) 
 }
 
 function logDatabaseErrors () {
   databaseConnection.on('error', console.error.bind(console, 'Hubo un error de conexión con la base de datos: '))
+}
+
+process.on('SIGTERM', () => {
+  console.log('SIGTERM signal received')
+  cleanupAndExit()
+})
+
+process.on('SIGINT', () => {
+  console.log('SIGINT signal received')
+  cleanupAndExit()
+})
+
+function cleanupAndExit () {
+  server.close(() => {
+    console.log('server connection closed')
+    databaseConnection.close()
+    console.log('database connection closed')
+    process.exit(0)
+  })
 }
