b58656636876042941a1098cbfef590ac03a460b
Gabriel Nunez <gnunez@aidacanarias.com>
2020-05-06 12:27:40 +0100

    rename

--- src/infrastructure/usersRepositoryMongoDB.ts
+++ src/infrastructure/usersRepositoryMongoDB.ts
@@ -3,23 +3,23 @@ import { User, NoUser } from '@/domain/models/user'
 import { Db } from 'mongodb'
 
 class UsersRepositoryMongoDB implements UsersRepository {
-  database: Db
+  databaseInstance: Db
 
   async create(userToCreate: User) {
-    const usersCollection = this.database.collection('users')
+    const usersCollection = this.databaseInstance.collection('users')
     const userData = userToCreate.getCredentials()
     await usersCollection.insertOne(userData)
   }
 
   async getBy(username: string) {
-    const usersCollection = this.database.collection('users')
+    const usersCollection = this.databaseInstance.collection('users')
     const returnedUser = await usersCollection.findOne({ username })
     if (!returnedUser) return new NoUser()
     return new User(returnedUser.username, returnedUser.password)
   }
 
   constructor(databaseInstance: Db) {
-    this.database = databaseInstance
+    this.databaseInstance = databaseInstance
   }
 }
 
