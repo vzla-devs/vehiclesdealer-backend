8f52ed7f93aff9bb52e54b9b6e2e4e90bd95aeba
Gabriel Nunez <gnunez@aidacanarias.com>
2020-05-07 09:51:24 +0100

    creating new route to login

--- src/api/routes/users.ts
+++ src/api/routes/users.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import { UsersFactory } from '@/infrastructure/factories/usersFactory'
 import { tryThisAndHandleAnyError } from '@/api/decorators'
 import { AddUserCommand } from '@/application/user/addUserAction'
+import { LoginUserCommand } from '@/application/user/loginUserAction'
 
 const router = express.Router()
 
@@ -13,6 +14,16 @@ router.post('/', tryThisAndHandleAnyError(async(req, res) => {
   const addUserAction = UsersFactory.getAddUserAction()
   await addUserAction.execute(command)
   res.sendStatus(201)
+}))
+
+router.post('/login', tryThisAndHandleAnyError(async(req, res) => {
+  const command: LoginUserCommand = {
+    username: req.body.username,
+    password: req.body.password
+  }
+  const loginUserAction = UsersFactory.getLoginUserAction()
+  await loginUserAction.execute(command)
+  res.sendStatus(200)
 }))
 
 export default router
