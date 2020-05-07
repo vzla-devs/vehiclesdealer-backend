5ca93dec895f6a2c3d62b149644b0ea7823afbf1
Gabriel Nunez <gnunez@aidacanarias.com>
2020-05-07 09:42:29 +0100

    creating factory method for new loginUserAction

--- src/infrastructure/factories/usersFactory.ts
+++ src/infrastructure/factories/usersFactory.ts
@@ -1,6 +1,7 @@
 import { getDatabaseConnection } from '@/infrastructure/factories/persistenceFactory'
 import { UsersRepositoryMongoDB } from '@/infrastructure/repositories/usersRepositoryMongoDB'
 import { AddUserAction } from '@/application/user/addUserAction'
+import { LoginUserAction } from '@/application/user/loginUserAction'
 
 export class UsersFactory {
   static getAddUserAction(): AddUserAction
@@ -8,5 +9,11 @@ export class UsersFactory {
     const databaseInstance = getDatabaseConnection().db
     const usersRepository = new UsersRepositoryMongoDB(databaseInstance)
     return new AddUserAction(usersRepository)
+  }
+
+  static getLoginUserAction(): LoginUserAction {
+    const databaseInstance = getDatabaseConnection().db
+    const usersRepository = new UsersRepositoryMongoDB(databaseInstance)
+    return new LoginUserAction(usersRepository)
   }
 }
