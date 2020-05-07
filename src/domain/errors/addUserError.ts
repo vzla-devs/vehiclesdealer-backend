72f3563177a56cdadb05deb8b62100f1f81e8106
Gabriel Nunez <gnunez@aidacanarias.com>
2020-05-07 20:36:49 +0100

    adding message to the error

--- src/domain/errors/addUserError.ts
+++ src/domain/errors/addUserError.ts
@@ -2,11 +2,11 @@ export class AddUserError extends Error {
   reason: AddUserErrorReason
 
   constructor(reason: AddUserErrorReason) {
-    super()
+    super(reason)
     this.reason = reason
   }
 }
 
 export enum AddUserErrorReason {
-  userAlreadyExists
+  userAlreadyExists = 'userAlreadyExists'
 }
