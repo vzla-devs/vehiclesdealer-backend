37c76165fa6a1bb9220892a2ffac206e8274f971
Gabriel Nunez <gnunez@aidacanarias.com>
2020-05-08 19:13:04 +0100

    removing mode

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const nodeExternals = require('webpack-node-externals')
 const path = require('path')
 
 module.exports = {
-  mode: 'development',
   entry: './src/api/index.ts',
   devtool: 'inline-source-map',
   target: 'node',
