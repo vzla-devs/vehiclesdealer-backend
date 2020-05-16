8aac8cc0807b022de1310f6f8f7d3a0bfb5e1a25
Gabriel Nunez <gnunez@aidacanarias.com>
2020-05-16 10:31:24 +0100

    simplifying logic

--- src/dealer/application/getServicesQuery.ts
+++ src/dealer/application/getServicesQuery.ts
@@ -2,10 +2,7 @@ import Service from '@/dealer/domain/serviceMixedModel'
 
 async function getAll () {
   const services = await Service.find({}).exec()
-  return services.sort((a, b) => {
-    if (a.spanish > b.spanish) return 1
-    return 0
-  })
+  return services
 }
 
 const getServicesQuery = {
