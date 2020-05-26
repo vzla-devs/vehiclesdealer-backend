369da75b0789d50ada9cb86d90dd257a96f28d3c
Gabriel Nunez <gnunez@aidacanarias.com>
2020-05-26 20:16:41 +0100

    rename

--- src/api/controllers/servicesController.ts
+++ src/api/controllers/servicesController.ts
@@ -1,13 +1,13 @@
 import express from 'express'
 import { tryThisAndHandleAnyError } from '@/shared/infrastructure/controllerDecorators'
-import { DealersFactory } from '@/dealer/infrastructure/dealerFactory'
+import { DealerFactory } from '@/dealer/infrastructure/dealerFactory'
 import { AddDealerServiceCommand } from '@/dealer/application/addDealerServiceAction'
 
 const router = express.Router()
 
 //TODO: create a unit test for this endpoint
 router.get('/', tryThisAndHandleAnyError(async(req, res) => {
-    const dealerServicesQuery = DealersFactory.GetDealerServicesQuery()
+    const dealerServicesQuery = DealerFactory.GetDealerServicesQuery()
     const services = await dealerServicesQuery.execute()
     res.status(200).send(services)
 }))
@@ -15,7 +15,7 @@ router.get('/', tryThisAndHandleAnyError(async(req, res) => {
 //TODO: create a unit test for this endpoint
 router.post('/', tryThisAndHandleAnyError(async(req, res) => {
     const command: AddDealerServiceCommand = { description: req.body.service }
-    const addDealerServicesAction = DealersFactory.AddDealerServiceAction()
+    const addDealerServicesAction = DealerFactory.AddDealerServiceAction()
     await addDealerServicesAction.execute(command)
     res.sendStatus(200)
 }))
